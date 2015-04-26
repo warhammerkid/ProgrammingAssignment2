4aac38aa6d2ba1e0be603a33ddd7a599c5010606
Stephen Augenstein <perl.programmer@gmail.com>
2015-04-26 13:20:11 -0400

    Implement makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions cache matrix inversions so that they only need to be computed
+## once for any given matrix.
 
-## Write a short comment describing this function
-
+## This creates a matrix wrapper that can store a cached inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL # Reset cached inverse, as matrix has changed
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Given an object from makeCacheMatrix, returns the inverse of the matrix.
+## This only performs the matrix inversion if it isn't cached already.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
